33c9ded8eb53294be39486db577d04348360db73
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-19 21:32:47 -0800

    Switched to material ui instead of react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
-
+import Router from 'react';
 function App() {
   return (
+
+    <Router>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </Router>
   );
 }
 
