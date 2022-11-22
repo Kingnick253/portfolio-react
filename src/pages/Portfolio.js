e869aa462d2312dd36f2dbbb589c8cc729ca0ced
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-21 22:35:14 -0800

    added basic style to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,9 +6,15 @@ export const Portfolio = () => {
         <div className=" grid grid-cols-2 gap-4" id = "portfolio" >
             {data.map(({id, title,image, description, repo, live}) => (
                 <div key={id} className ="p-4 bg-gray-300">
+                    <h1 className="font-bold">{title}</h1>
                     {image ? <img src = {image} />: null}
-                    {title}
+                    <div>
+                    <a className=" hover:bg-slate-600" href={repo} target="_blank" rel="noreferrer">Repo</a>
+                    </div>
+                    <div>
+                    <a  className=" hover:bg-slate-600" href={live} target="_blank" rel="noreferrer">Live</a>
                     
+                    </div>
             
                 </div>
             
